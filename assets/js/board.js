6cddca4dd3a5db957deb5cc693587cfefcab22a2
Hudson Carolino <hudson@kenzie.com.br>
2021-02-02 11:02:46 -0300

    modelagem inicial da classe board

---
+++ assets/js/board.js
@@ -0,0 +1,23 @@
+class Board{
+    constructor(id,nome,secoes){
+        this._id     = id;
+        this._nome   = nome;
+        this._secoes = secoes;
+    }
+
+    set nome(nome){
+        this._nome = nome
+    }
+
+    get  nome(nome){
+        this._nome = nome
+    }
+
+    criarSecao(nome){
+
+    }
+
+    removerSecao(posicao){
+
+    }
+}
